d4f87704ac8c4e087614816e9e992bc79543dcc3
Eric Furukawa <eric.furukawa@gmail.com>
2021-12-18 19:13:01 -0800

    fixed bug with post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,25 +3,25 @@ const fs = require('fs');
 
 router.get ('/api/notes', (req, res) => {
     const notes = JSON.parse(fs.readFileSync('db/db.json'));
-    res.json(notes);
+    return res.json(notes);
 });
 
 
 //post a new note
-router.post('/api/notes', (res, req) => {
-    let notes = JSON.parse(fs.readFileSync('db/db/json'));
+router.post('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
     const newNote = {
-        id: notes[notes.length - 1].id + 1,
+        id: notes[notes.length].id,
         title: req.body.title,
         text: req.body.text
     };
 
     console.log(newNote);
     notes.push(newNote);
-    fs. writeFileSync('./db/db.json',
-    JSONs.stringify(notes));
+    fs.writeFileSync('./db/db.json',
+    JSON.stringify(notes));
 
-    res.json(notes);
+    return res.json(notes);
 
 });
 
